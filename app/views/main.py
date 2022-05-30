9db3f6dfec35ea9a95af9eaf61a20a91b12252e2
Andy-Ray <13idol13@gmail.com>
2022-05-30 18:31:54 +0300

    changed index

--- app/views/main.py
+++ app/views/main.py
@@ -1,8 +1,23 @@
-from flask import render_template, Blueprint
+from flask import Blueprint, redirect
+from flask.helpers import url_for
+from app.logger import log
+from flask_login import current_user
+from app.models.user import AnonymousUser
+
 
 main_blueprint = Blueprint("main", __name__)
 
 
 @main_blueprint.route("/")
 def index():
-    return render_template("index.html")
+
+    log(
+        log.INFO,
+        "User [] on index page",
+    )
+
+    # return redirect(url_for("auth.login"))
+    if isinstance(current_user, AnonymousUser):
+        return redirect(url_for("auth.login"))
+
+    return redirect(url_for("user.index"))
