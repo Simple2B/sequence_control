bd6579c1c1cc2f1bd751f05a91a0e711dabf4358
Andy-Ray <13idol13@gmail.com>
2022-06-01 21:38:39 +0300

    commented for now

--- app/views/user.py
+++ app/views/user.py
@@ -1,8 +1,6 @@
-from flask import render_template, url_for, redirect, Blueprint, request, flash
+from flask import render_template, Blueprint
 from flask_login import login_required
-from app.forms.auth import PmRegistrationForm
 from app.logger import log
-from app.models import User
 
 user_blueprint = Blueprint("user", __name__)
 
@@ -19,27 +17,27 @@ def index():
     return render_template("users.html")
 
 
-@user_blueprint.route("/project_manager_add", methods=["GET", "POST"])
-@login_required
-# @role_required(roles=[User.RoleType.admin])
-def project_manager_add():
-    log(
-        log.INFO,
-        "User [] on project_manager_add",
-    )
-    form = PmRegistrationForm(request.form)
-    if form.validate_on_submit():
-        user = User(
-            username=form.username.data,
-            email=form.email.data,
-            password=form.password.data,
-            company=form.company_name.data,
-            position=form.position.data,
-            sc_role=User.RoleType.project_manager,
-        )
-        user.save()
-        flash("Registration successful.", "success")
-        return redirect(url_for("define.define"))
-    elif form.is_submitted():
-        flash("The given data was invalid.", "danger")
-    return render_template("project_manager_add.html", form=form)
+# @user_blueprint.route("/project_manager_add", methods=["GET", "POST"])
+# @login_required
+# # @role_required(roles=[User.RoleType.admin])
+# def project_manager_add():
+#     log(
+#         log.INFO,
+#         "User [] on project_manager_add",
+#     )
+#     form = PmRegistrationForm(request.form)
+#     if form.validate_on_submit():
+#         user = User(
+#             username=form.username.data,
+#             email=form.email.data,
+#             password=form.password.data,
+#             company=form.company_name.data,
+#             position=form.position.data,
+#             sc_role=User.RoleType.project_manager,
+#         )
+#         user.save()
+#         flash("Registration successful.", "success")
+#         return redirect(url_for("define.define"))
+#     elif form.is_submitted():
+#         flash("The given data was invalid.", "danger")
+#     return render_template("project_manager_add.html", form=form)
