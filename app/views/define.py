a9ef585cf3dd802b4e28b096e27657311e64e6ea
Andy-Ray <13idol13@gmail.com>
2022-06-01 14:11:45 +0300

    added: paroject_manager_add

--- app/views/define.py
+++ app/views/define.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from app.logger import log
 
 
 define_blueprint = Blueprint("define", __name__)
@@ -6,4 +7,9 @@ define_blueprint = Blueprint("define", __name__)
 
 @define_blueprint.route("/define")
 def define():
+    log(
+        log.INFO,
+        "User [] define",
+    )
+
     return render_template("define.html")
--- app/views/user.py
+++ app/views/user.py
@@ -1,6 +1,8 @@
-from flask import render_template, Blueprint
+from flask import render_template, url_for, redirect, Blueprint, request, flash
 from flask_login import login_required
+from app.forms.auth import PmRegistrationForm, WPMRegistrationForm
 from app.logger import log
+from app.models import User
 
 user_blueprint = Blueprint("user", __name__)
 
@@ -15,3 +17,29 @@ def index():
     )
 
     return render_template("users.html")
+
+
+@user_blueprint.route("/project_manager_add", methods=["GET", "POST"])
+@login_required
+# @role_required(roles=[User.RoleType.admin])
+def project_manager_add():
+    log(
+        log.INFO,
+        "User [] on project_manager_add",
+    )
+    form = PmRegistrationForm(request.form)
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+            company=form.company_name.data,
+            position=form.position.data,
+            sc_role=User.RoleType.project_manager,
+        )
+        user.save()
+        flash("Registration successful.", "success")
+        return redirect(url_for("define.define"))
+    elif form.is_submitted():
+        flash("The given data was invalid.", "danger")
+    return render_template("project_manager_add.html", form=form)
