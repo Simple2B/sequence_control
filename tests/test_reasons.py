72b0486b25a8304080536c4c6b500d930a0f6e9c
Andy-Ray <13idol13@gmail.com>
2022-06-03 16:57:56 +0300

    added: reason model

---
+++ tests/test_reasons.py
@@ -0,0 +1,23 @@
+import pytest
+from app import db, create_app
+from wsgi import add_reasons
+
+
+@pytest.fixture
+def client():
+    app = create_app(environment="testing")
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        app_ctx = app.app_context()
+        app_ctx.push()
+        db.drop_all()
+        db.create_all()
+        yield client
+        db.session.remove()
+        db.drop_all()
+        app_ctx.pop()
+
+
+def test_add_reasons_cli(client):
+    add_reasons()
