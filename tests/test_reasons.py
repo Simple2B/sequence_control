86fc17ab7d649326caddeff0dc696a714b9fc99d
Andy-Ray <13idol13@gmail.com>
2022-06-04 16:21:58 +0300

    added test

--- tests/test_reasons.py
+++ tests/test_reasons.py
@@ -1,6 +1,8 @@
 import pytest
 from app import db, create_app
 from wsgi import add_reasons
+from tests.utils import create_admin_register, login
+from app.models import Reason
 
 
 @pytest.fixture
@@ -21,3 +23,27 @@ def client():
 
 def test_add_reasons_cli(client):
     add_reasons()
+
+
+def test_add_reasons(client):
+    create_admin_register("admin")
+
+    login(client, "admin")
+    REASON_NAME = "Test_REASON"
+
+    reasons = Reason.query.all()
+
+    assert not reasons
+
+    client.post(
+        "/reason_add",
+        data=dict(
+            name=REASON_NAME,
+        ),
+        follow_redirects=True,
+    )
+    reasons = Reason.query.all()
+
+    assert len(reasons) == 1
+    reason: reason = reasons[0]
+    assert reason.name == REASON_NAME
