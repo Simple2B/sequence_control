306a0af882ab1f111c44c00f9ace98f74b37c8c7
Andy-Ray <13idol13@gmail.com>
2022-06-04 16:38:14 +0300

    moved test client to separate file

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,23 +1,7 @@
-import pytest
-
-from app import db, create_app
 from tests.utils import create_admin_register, login, logout
 
-
-@pytest.fixture
-def client():
-    app = create_app(environment="testing")
-    app.config["TESTING"] = True
-
-    with app.test_client() as client:
-        app_ctx = app.app_context()
-        app_ctx.push()
-        db.drop_all()
-        db.create_all()
-        yield client
-        db.session.remove()
-        db.drop_all()
-        app_ctx.pop()
+# flake8: noqa F401
+from .conftest import client
 
 
 def test_auth_pages(client):
