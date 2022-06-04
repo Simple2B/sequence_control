3f1510b02c1426e2b8982c2fd91b1663739d378a
Andy-Ray <13idol13@gmail.com>
2022-06-04 16:52:59 +0300

    from .conftest import client

--- tests/test_reasons.py
+++ tests/test_reasons.py
@@ -1,24 +1,10 @@
-import pytest
-from app import db, create_app
 from wsgi import add_reasons
 from tests.utils import create_admin_register, login
 from app.models import Reason
 
 
-@pytest.fixture
-def client():
-    app = create_app(environment="testing")
-    app.config["TESTING"] = True
-
-    with app.test_client() as client:
-        app_ctx = app.app_context()
-        app_ctx.push()
-        db.drop_all()
-        db.create_all()
-        yield client
-        db.session.remove()
-        db.drop_all()
-        app_ctx.pop()
+# flake8: noqa F401
+from .conftest import client
 
 
 def test_add_reasons_cli(client):
