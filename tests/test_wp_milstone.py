d7d439888e57f7b197247232b1c75c1cf04fcdef
Andy-Ray <13idol13@gmail.com>
2022-06-06 10:52:49 +0300

    test..

--- tests/test_wp_milstone.py
+++ tests/test_wp_milstone.py
@@ -1,7 +1,5 @@
-import pytest
-from app import db, create_app
-from tests.utils import create_wp_manager, login
-from app.models import WPMilestone, wp_milestone
+from tests.utils import create_manager, login, logout
+from app.models import WPMilestone, User, ProjectMilestone
 from datetime import datetime, timedelta
 
 
@@ -11,7 +9,40 @@ from .conftest import client
 
 def test_add_wp_milestone(client):
 
-    create_wp_manager("wp_manager")
+    create_manager(
+        "manager", role=User.Role.project_manager, email="manager@manager.com"
+    )
+
+    login(client, "manager")
+    MILESTONE_NAME = "Test_Milestone"
+    MILESTONE_DESCRIPTION = "S-2-B"
+    MILESTONE_BASELINE = (datetime.now() + timedelta(days=30)).date()
+    PROJECT_ID = 1
+    milestones = ProjectMilestone.query.all()
+
+    assert not milestones
+
+    response = client.post(
+        "/milestone_add",
+        data=dict(
+            name=MILESTONE_NAME,
+            description=MILESTONE_DESCRIPTION,
+            baseline_date=MILESTONE_BASELINE,
+            project_id=PROJECT_ID,
+        ),
+        follow_redirects=True,
+    )
+    assert b"Milestone Registration  is successful." in response.data
+    milestones = ProjectMilestone.query.all()
+
+    assert len(milestones) == 1
+    milestone: WPMilestone = milestones[0]
+    assert milestone.name == MILESTONE_NAME
+    assert milestone.description == MILESTONE_DESCRIPTION
+
+    logout(client)
+
+    create_manager("wp_manager", role=User.Role.wp_manager)
 
     login(client, "wp_manager")
     WP_MILESTONE_NAME = "Test_Project"
@@ -28,7 +59,7 @@ def test_add_wp_milestone(client):
             name=WP_MILESTONE_NAME,
             description=WP_MILESTONE_DESCRIPTION,
             baseline_date=WP_MILESTONE_BASELINE,
-            project_milestone_id=PROJECT_MILESTONE_ID,
+            project_milestone_id=milestone.id,
         ),
         follow_redirects=True,
     )
@@ -39,6 +70,7 @@ def test_add_wp_milestone(client):
     wp_milestone: WPMilestone = wp_milestones[0]
     assert wp_milestone.name == WP_MILESTONE_NAME
     assert wp_milestone.description == WP_MILESTONE_DESCRIPTION
+    assert wp_milestone.project_milestone_id == milestone.id
 
     # test adding milestone with existing name
     WP_MILESTONE_DESCRIPTION2 = "S-3-B"
