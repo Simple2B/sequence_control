35f995f8830af9b341952c2fa3fb924207858442
Andy-Ray <13idol13@gmail.com>
2022-06-09 22:47:03 +0300

    viewer_add for diferenet roles

--- app/views/user/viewer.py
+++ app/views/user/viewer.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, redirect, Blueprint, request, flash, session
+from flask import render_template, url_for, redirect, Blueprint, flash, session
 from flask_login import current_user, login_required
-from app.forms import WPMRegistrationForm
+from app.forms import PmRegistrationForm
 from app.logger import log
-from app.models import User, WorkPackage
+from app.models import User, ProjectViewer
 from app.controllers import role_required
 
 viewer_blueprint = Blueprint("viewer", __name__)
@@ -12,34 +12,31 @@ viewer_blueprint = Blueprint("viewer", __name__)
 @login_required
 @role_required(roles=[User.Role.admin, User.Role.project_manager])
 def viewer_add():
-    log(log.INFO, "User [%d] on viewer_add", current_user.id)
-    form = WPMRegistrationForm()
-    if current_user.role == User.Role.admin:
-        form.wp_responsible.choices = [
-            (wp.id, wp.number)
-            for wp in WorkPackage.query.filter_by(deleted=False).all()
-        ]
-    else:
-        form.wp_responsible.choices = [
-            (wp.id, wp.number)
-            for wp in WorkPackage.query.filter_by(
-                deleted=False,
-                manager_id=current_user.id,
-                project_id=session["project_id"],
-            ).all()
-        ]
+    log(log.INFO, "[viewer_add]User [%d] on ", current_user.id)
+    form = PmRegistrationForm()
     if form.validate_on_submit():
+        project_id = session["project_id"]
         user = User(
             username=form.username.data,
             email=form.email.data,
             password=form.password.data,
             company=form.company_name.data,
             position=form.position.data,
-            wp_responsible=form.wp_responsible.data,
             role=User.Role.viewer,
             subordinate_id=current_user.id,
         )
         user.save()
+        # if project manager creating viewer we can assign him to current project
+        if current_user.role == User.Role.project_manager:
+            ProjectViewer(project_id=project_id, viewer_id=user.id).save()
+            log(
+                log.INFO,
+                "[viewer_add.validate_on_submit] User [%d] created user [%d] for project[%d]",
+                current_user.id,
+                user.id,
+                project_id,
+            )
+
         flash("Registration successful.", "success")
         return redirect(url_for("define.define"))
     elif form.is_submitted():
