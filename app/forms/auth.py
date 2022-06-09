9ae7924ee6802c9d22d834d49affa3e8aaddd783
Andy-Ray <13idol13@gmail.com>
2022-06-09 18:36:48 +0300

    removed unused roles

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -42,11 +42,6 @@ class RegistrationForm(FlaskForm):
 
 
 class PmRegistrationForm(RegistrationForm):
-    ROLES = [
-        (User.Role.project_manager.value, User.Role.project_manager.name),
-        (User.Role.wp_manager.value, User.Role.wp_manager.name),
-        (User.Role.viewer.value, User.Role.viewer),
-    ]
 
     position = StringField("Position", validators=[DataRequired(), Length(2, 30)])
 
