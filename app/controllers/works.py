f5daaf724b1c8395c0994b2612e1e7134c6ddf76
Andy-Ray <13idol13@gmail.com>
2022-06-14 13:28:38 +0300

    removed old code

--- app/controllers/works.py
+++ app/controllers/works.py
@@ -1,8 +1,6 @@
 from flask import session
 from flask_login import current_user
-
-from app.logger import log
-from app.models import User, Work, PlanDate, WorkPackage
+from app.models import User, Work, WorkPackage
 
 
 def get_works_for_project(ppc_type: Work.PpcType, type: Work.Type = None):
@@ -16,9 +14,6 @@ def get_works_for_project(ppc_type: Work.PpcType, type: Work.Type = None):
                 return Work.query.filter_by(
                     deleted=False, wp_id=wp_id, ppc_type=ppc_type
                 )
-        else:
-            plan_dates_ids = []
-            log(log.ERROR, "[get_works_for_project] No wp_id in session!")
     else:
         project_id = session.get("project_id")
         if project_id:
@@ -35,8 +30,3 @@ def get_works_for_project(ppc_type: Work.PpcType, type: Work.Type = None):
                 else Work.query.filter_by(deleted=False, ppc_type=ppc_type)
             )
             return query.filter(Work.wp_id.in_(wp_ids))
-        else:
-            plan_dates_ids = []
-            log(log.ERROR, "[get_works_for_project] No project_id in session!")
-
-    return PlanDate.query.filter(PlanDate.id.in_(plan_dates_ids))
