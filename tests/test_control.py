fa0a62f6610f435678b02d997d7b4a90949d2dff
Andy-Ray <13idol13@gmail.com>
2022-06-16 14:24:33 +0300

    test

--- tests/test_control.py
+++ tests/test_control.py
@@ -1,7 +1,7 @@
 # import os
-from datetime import timedelta, datetime
+from datetime import datetime
 from flask.testing import FlaskClient
-from app.models import Work, PlanDate
+from app.models import Work, PlanDate, Reason
 from .utils import create_work_package
 
 
@@ -23,6 +23,7 @@ def test_control(manager: FlaskClient):
 
     atp_work: Work = Work.query.first()
     assert atp_work
+    assert not atp_work.reason_id
     response = manager.get(
         "/control",
         follow_redirects=True,
@@ -37,3 +38,20 @@ def test_control(manager: FlaskClient):
     assert b"test_ref" in response.data
     assert b"2003-09-25" in response.data
     assert b"TEST_CONTRACTOR" in response.data
+
+    REASONS = [
+        "Outstanding Design, Consultant, Subcontractor & Supplier design Information, TQ's and RFI's, Design approvals"
+    ]
+    for reason in REASONS:
+        Reason(name=reason).save()
+
+    reason: Reason = Reason.query.first()
+    assert reason
+
+    response = manager.post(
+        "/work_select_reason",
+        data=dict(work_id=atp_work.id, reason_id=reason.id),
+        follow_redirects=True,
+    )
+
+    assert atp_work.reason_id == reason.id
