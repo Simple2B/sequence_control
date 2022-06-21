0a420f6819733ee8f645ac7d90bf125d62ea8ae6
Andy-Ray <13idol13@gmail.com>
2022-06-21 18:45:08 +0300

    changed: select WP on creation WP Manager

--- app/forms/work_package.py
+++ app/forms/work_package.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, ValidationError, SelectField
+from wtforms import StringField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Length
 
-from app.models import WorkPackage, User
+from app.models import WorkPackage
 
 
 class WorkPackageForm(FlaskForm):
@@ -11,17 +11,7 @@ class WorkPackageForm(FlaskForm):
     contractor_name = StringField(
         "Contractor Name", validators=[DataRequired(), Length(2, 30)]
     )
-    wp_manager = SelectField("WP Manager", coerce=int, choices=[])
     submit = SubmitField("Submit")
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.wp_manager.choices = [
-            (user.id, user.username)
-            for user in User.query.filter_by(
-                deleted=False, role=User.Role.wp_manager, wp_id=None
-            ).all()
-        ]
 
     def validate_number(form, field):
         if WorkPackage.query.filter_by(number=field.data).first() is not None:
--- app/views/user/wp_manager.py
+++ app/views/user/wp_manager.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, redirect, Blueprint, flash
+from flask import render_template, url_for, redirect, Blueprint, flash, session
 from flask_login import current_user, login_required
-from app.forms import PmRegistrationForm
+from app.forms import WPMRegistrationForm
 from app.logger import log
-from app.models import User
+from app.models import User, ProjectWPManager
 from app.controllers import role_required
 
 wp_manager_blueprint = Blueprint("wp_manager", __name__)
@@ -13,7 +13,7 @@ wp_manager_blueprint = Blueprint("wp_manager", __name__)
 @role_required(roles=[User.Role.project_manager])
 def wp_manager_add():
     log(log.INFO, "User [%d] on wp_manager_add", current_user.id)
-    form = PmRegistrationForm()
+    form = WPMRegistrationForm()
     if form.validate_on_submit():
         user = User(
             username=form.username.data,
@@ -23,8 +23,11 @@ def wp_manager_add():
             position=form.position.data,
             role=User.Role.wp_manager,
             subordinate_id=current_user.id,
+            wp_id=form.wp_responsible.data,
         )
         user.save()
+        project_id = session["project_id"]
+        ProjectWPManager(project_id=project_id, wp_manager_id=user.id).save()
         flash("Registration successful.", "success")
         return redirect(url_for("define.define"))
     elif form.is_submitted():
