d43e7a7ee2886802cd2442c4fd3314ee7c0164e2
Andy-Ray <13idol13@gmail.com>
2022-06-22 15:19:58 +0300

    tests

--- tests/test_control.py
+++ tests/test_control.py
@@ -1,11 +1,25 @@
 # import os
 from datetime import datetime, timedelta
 from flask.testing import FlaskClient
+from sqlalchemy import desc
 from app.models import Work, PlanDate, Reason
 from .utils import create_work_package
 
 
 def test_control(manager: FlaskClient):
+    # adding reasons
+    REASONS = [
+        "Outstanding Design",
+        "Consultant",
+        "Subcontractor & Supplier design Information",
+        "TQ's and RFI's",
+        "Design approvals",
+    ]
+    for reason in REASONS:
+        Reason(name=reason).save()
+
+    reason: Reason = Reason.query.first()
+    assert reason
     # creating wp and work
     wp_id = create_work_package(3)
     work = Work(
@@ -20,6 +34,7 @@ def test_control(manager: FlaskClient):
     PlanDate(
         date=date,
         work_id=work.id,
+        user_id=2,
     ).save()
 
     atp_work: Work = Work.query.first()
@@ -35,78 +50,41 @@ def test_control(manager: FlaskClient):
     assert b"Type" in response.data
     assert b"Deliverable" in response.data
     assert b"Ref No." in response.data
-    assert b"Responsible" in response.data
     assert b"ATP1" in response.data
     assert b"test_deliverable" in response.data
     assert b"test_ref" in response.data
     assert b"2003-09-25" in response.data
-    assert b"TEST_CONTRACTOR" in response.data
 
-    # adding reasons
-    REASONS = [
-        "Outstanding Design, Consultant, Subcontractor & Supplier design Information, TQ's and RFI's, Design approvals"
-    ]
-    for reason in REASONS:
-        Reason(name=reason).save()
-
-    reason: Reason = Reason.query.first()
-    assert reason
-
-    # check for select reason
-    assert not atp_work.reason_id
-
-    response = manager.post(
-        "/work_select_reason",
-        data=dict(work_id=atp_work.id, reason_id=reason.id),
-        follow_redirects=True,
-    )
-
-    assert atp_work.reason_id == reason.id
-
-    # check for date edit
+    # checking old current information
     old_date = atp_work.latest_date
     old_version = atp_work.latest_date_version
     assert old_version == 1
 
-    response = manager.get(
-        f"/edit_work_date/{atp_work.id}",
-        follow_redirects=True,
-    )
-    assert response
-
+    # simulating new reforecast
     new_date = (old_date + timedelta(days=10)).date()
+    reason = "Consultant"
+    note = "THIS IS AWESOME NOTE !!!"
+    responsible = "TEST_CONTRACTOR"
 
     response = manager.post(
-        f"/edit_work_date/{atp_work.id}",
+        f"/reforecast/{atp_work.id}",
         data=dict(
-            new_plan_date=new_date,
+            new_plan_date=new_date, reason=reason, note=note, responsible=responsible
         ),
         follow_redirects=True,
     )
-    assert response
 
+    # checking if information have changed
     atp_work: Work = Work.query.filter(Work.ppc_type == Work.PpcType.atp).first()
+    plan_dates: PlanDate = (
+        PlanDate.query.filter_by(work_id=atp_work.id)
+        .order_by(desc(PlanDate.version))
+        .all()
+    )
+    assert len(plan_dates) == 2
     assert atp_work.latest_date.date() == new_date
     assert atp_work.latest_date_version == 2
-
-    # check edit notes
-    assert not atp_work.note
-    NEW_NOTE = "THIS IS AWESOME NOTE !!!"
-    response = manager.post(
-        f"/edit_work_note/{atp_work.id}",
-        data=dict(reference=atp_work.reference, note=NEW_NOTE),
-        follow_redirects=True,
-    )
-
-    assert atp_work.note == NEW_NOTE
-
-    # check edit notes
-    assert not atp_work.complete
-
-    response = manager.post(
-        "/work_select_complete",
-        data=dict(work_id=atp_work.id, complete="yes"),
-        follow_redirects=True,
-    )
-
-    assert atp_work.complete == "yes"
+    plan_date: PlanDate = plan_dates[0]
+    assert plan_date.reason == reason
+    assert plan_date.responsible == responsible
+    assert plan_date.note == note
